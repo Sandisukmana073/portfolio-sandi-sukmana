fd0bf7c7c8c1ce950ccd3c8893cdfa81d23163c9
sandi sukmana <158050198+Sandisukmana073@users.noreply.github.com>
2025-07-28 14:37:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Kosong dulu, bisa ditambah animasi scroll, typing effect, dsb nanti<script>
+  function toggleDescription(id) {
+    const desc = document.getElementById(id);
+    desc.style.display = desc.style.display === 'none' ? 'block' : 'none';
+  }
+
+
